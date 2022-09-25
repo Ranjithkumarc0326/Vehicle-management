59dde2743379573c6b1391b18ede30a538467cb1
Ranjithkumarc0326 <ranjithkumarc2001@gmail.com>
2022-09-25 19:16:52 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<?php 
+    $connection=mysqli_connect("localhost","root","","vehicle management");
+
+    session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>vehicle management system</title>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+     <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css"> 
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="animate.css">
+    <link rel="stylesheet" href="style.css">
+    
+
+ 
+</head> 
+<style>
+    
+.parallax {
+    /* The image used */
+    background-image: url("vmi.jpg");
+    height: 100%;
+
+    /* Set a specific height */
+    min-height: 700px; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    }
+    
+.parallax1 {
+    /* The image used */
+    background-image: url("pexels-photo-280310 .jpeg");
+    height: 100%;
+
+    /* Set a specific height */
+    min-height: 600px; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    } 
+    
+    .navbar-fixed-top.scrolled {
+       background-color: ghostwhite;
+      transition: background-color 200ms linear;
+    }
+
+</style>
+<body data-spy="scroll" data-target=".navbar" data-offset="50" onload="myFunction()"> 
+   
+   
+ 
+       
+    
+     
+     
+     
+    <!--
+    <div>  
+       <div class="jumbotron">
+          <h2 class="animated bounce">Ruet Vehicle Management</h2>      
+          <p>A management system where you can easily manage vehicles.</p>
+        </div>
+           
+       
+        
+    </div> 
+    -->
+    <div class="parallax foo">
+       <?php include 'navbar.php';?>
+    
+        <div class="hero-text" style="font-size:50px text-align: center; position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);color: white;">
+           
+            <h1 class="animated rubberBand" >JIT VEHICLE MANAGEMENT SYSTEM</h1>
+            <p>A management system where you can easily manage vehicles</p>
+            
+            <?php if(isset($_SESSION['username'])==true) { ?>
+            <a class="btn btn-success" style="text-align: center" href="booking.php">Book a Vehicle</a>
+            
+            <?php } else{  ?>
+            <a class="btn btn-success" style="text-align: center" href="login.php">LOGIN TO BOOK A VEHICLE</a> 
+            <?php } ?>
+            
+          </div>
+    </div>                 
+    
+    <div>
+       <br><br>
+        <div id="bus_route" class="container">
+          <div class="page-header">
+            <h1 style="text-align: center">Bus Route</h1>      
+          </div> 
+          <div class="row">
+              <div class="col-md-6 foo">
+                <p><b>JIT BUS GIVES YOU A ULTIMATE COMFORT WITH ALL SAFTEY MEASURES WITH A LIFE TIME MEMORIES.</b></p>
+                <img src="pexels-photo-136739.jpeg" class="img-responsive" >  
+              </div>
+              <div class="col-md-6">
+                  <br>
+                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3890.037345580017!2d77.50952201461051!3d12.840863290941714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae155daf255555%3A0xb0d9badee8173f84!2sJyothy%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1642476905107!5m2!1sen!2sin" width="500" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+                   <p>The Bus Route</p>
+              </div>
+          </div>       
+        </div>
+        
+        <br>
+        <div class="page-header">
+            <h1 style="text-align: center">Driver</h1>      
+        </div>
+        <div class="parallax1"></div>
+        <div id="driver" class="container">
+          
+          <div class="row">
+              <div class="col-md-12">
+                  <p style="font-size: 20px;">The driver of JIT are very punctual and they provides great service. Every one of them is professional and great at their jobs by providing best comfort to customers </p>
+                  
+              </div>
+          </div>
+               
+        </div>
+        
+        
+        <div id="bus" class="container">
+          <div class="page-header">
+            <h1 style="text-align: center">Bus </h1>      
+          </div> 
+          <div class="row">
+              <div class="col-md-6">
+                
+                <img src="pexels-photo-385998.jpeg" class="img-responsive" >  
+              </div>
+              <div class="col-md-6 foo1">
+                  <p style="font-size:20px;"><b>In JIT  we have around ten buses and each one of it is well maintained. These buses goes in different direction of the city and can also be hired for daily usages.</b></p>
+              </div>
+              
+          </div>       
+        </div>
+        
+        
+          
+          <p></p>      
+                
+        </div>  
+        
+        <footer style="background-color: #2f2f2f;
+          color: #fff; padding-top: 70px;
+          padding-bottom: 70px;" class="container-fluid text-center">
+                <p>All rights reserved by @SAMARTHA @RANJITH </p> 
+        </footer>
+        
+        
+        
+        
+    
+    
+    
+<script>
+    $(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $a= $(".parallax");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $a.height());
+  });
+}); 
+    
+    </script>    
+  
+  
+  <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+  
+  
+  <script>
+        window.sr = ScrollReveal();
+        sr.reveal('.foo', { duration: 800 });
+        sr.reveal('.foo1', { duration: 800,origin: 'top'});
+    </script>
+    
+</body>
+</html>
